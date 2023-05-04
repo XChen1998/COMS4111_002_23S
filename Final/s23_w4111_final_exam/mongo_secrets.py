296199fda68841b84d69dd8f99e0a63aec4310d9
Ziggy Chen <90239395+XChen1998@users.noreply.github.com>
2023-05-04 17:11:23 -0400

    Working on final coding (M1 Max).

---
+++ Final/s23_w4111_final_exam/mongo_secrets.py
@@ -0,0 +1,2 @@
+password = "L4JNI5NRbEq6DsNU"
+mongo_atlas_url = f"mongodb+srv://xc2605:{password}@s23-w4111.cmuk4kg.mongodb.net/?retryWrites=true&w=majority"
---
+++ Final/s23_w4111_final_exam/neo4j_secrets.py
@@ -0,0 +1,3 @@
+aura_url = 'neo4j+s://edc9eb2d.databases.neo4j.io'
+aura_user = 'neo4j'
+aura_pw = 'OgaFeWWhPZ3kKbaoHPJh4fZTp5Ip3RDtwi70qqU8w8g'
